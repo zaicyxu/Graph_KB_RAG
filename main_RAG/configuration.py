dd41275c3dc406a52743f316863051b32d9f7b2b
Hersmion Rain <106503993+zaicyxu@users.noreply.github.com>
2025-10-21 11:30:31 +0200

    Clear sensitive data from configuration.py
    Removed sensitive information from configuration file.

--- main_RAG/configuration.py
+++ main_RAG/configuration.py
@@ -7,16 +7,14 @@
 """
 
 # Gemini API Key
-GEMINI_API_KEY = "AIzaSyBdTPqt4RpQvOc676Z1v_OuEkDsqhrJd9k"
-# GEMINI_API_KEY = "AIzaSyCg8PHoIJhvIukiooFg6b7K2Bi8r-EkhEQ"
-# GEMINI_API_KEY = "AIzaSyC2LaeLTBOj-qISv_OxdKJPUQzwEbJ595Q"
+GEMINI_API_KEY = 
+
 
 # Neo4j Database Credentials
-NEO4J_URI = "bolt://localhost:7687"
-# NEO4J_BLOOM_URI = "neo4j://localhost:7687"
-NEO4J_BLOOM_URI = "http://localhost:7474/bloom"
-NEO4J_USER = "neo4j"
-NEO4J_PASSWORD = "12345678"
+NEO4J_URI = 
+NEO4J_BLOOM_URI = 
+NEO4J_USER = 
+NEO4J_PASSWORD = 
 
 # Key entity types and relationships
 KEY_ENTITIES = {
@@ -33,4 +31,5 @@ RELATIONSHIP_MAPPING = {
     "Work_on": "Service",
     "Sub_Business": "Service",
     "Sub_Industry": "Industry"
+
 }
